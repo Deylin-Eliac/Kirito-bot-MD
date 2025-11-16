17edb902f2a92629b31f9210f77628d57be2e227
Deylin <deylinbo91@gmail.com>
2025-11-16 14:51:05 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ say('Bot', {
   align: 'left',
   gradient: ['green', 'white']
 });
-say('developed by 𝐀𝐥𝐞𝐣𝐚𝐧𝐝𝐫𝐨', {
+say('developed by Eliac', {
   font: 'console',
   align: 'center',
   colors: ['cyan', 'magenta', 'yellow']
